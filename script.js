31adbb5838c63ba418fab65f513d036aa40b1981
Kylan <kylan.zastrow@gmail.com>
2022-08-03 11:41:39 -0600

    Phone size nav bar.
    The basic design of the phone size navigation bar.

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "250px";
+    document.getElementById("icon").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0";
+    document.getElementById("icon").style.marginLeft = "0";
+  }
