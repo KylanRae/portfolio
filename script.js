39f028e4d4fab83a829dcde87cef3db6a371bacb
Kylan <kylan.zastrow@gmail.com>
2022-08-03 13:20:03 -0600

    Altered mobile navigation bar.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Opening navigation bar (mobile)
 function openNav() {
-    document.getElementById("myNav").style.width = "250px";
-    document.getElementById("icon").style.marginLeft = "250px";
+    document.getElementById("myNav").style.width = "10em";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
+  //Close navigation bar (mobile)
   function closeNav() {
     document.getElementById("myNav").style.width = "0";
-    document.getElementById("icon").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
   }
