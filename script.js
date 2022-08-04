b05c5750feb3b3041a48a997fca9d2de2c096b3e
Kylan <kylan.zastrow@gmail.com>
2022-08-04 08:09:13 -0600

    Nav Bar Works
    Unsuccessful in getting the background to darken. Decided to have nav bar take up large amounts of the screen and do nothing else.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //Opening navigation bar (mobile)
 function openNav() {
-    document.getElementById("myNav").style.width = "33%";
-    document.overlay.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("myNav").style.width = "66%";
 }
 
 //Close navigation bar (mobile)
 function closeNav() {
     document.getElementById("myNav").style.width = "0";
-    document.overlay.style.backgroundColor = "white";
 }
