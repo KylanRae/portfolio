a695f3d33576a311c2b366288d1e1dfc6a0c90a7
Kylan <kylan.zastrow@gmail.com>
2022-08-03 21:13:46 -0600

    Final changes for the day
    - Attempted to darken whole page while menu is open... (unsucessful)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Opening navigation bar (mobile)
 function openNav() {
     document.getElementById("myNav").style.width = "33%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.overlay.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 //Close navigation bar (mobile)
 function closeNav() {
     document.getElementById("myNav").style.width = "0";
-    document.body.style.backgroundColor = "white";
+    document.overlay.style.backgroundColor = "white";
 }
